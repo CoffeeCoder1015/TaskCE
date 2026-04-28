310d3d9abdf8e8cbc02b8ff0a404f05277b3c371
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-04-28 11:47:29 -0700

    base evaluator

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,4 +1,69 @@
-from evaluation.evalConfig import EvalConfig
+from collections import Counter
 
-def Evaluate(model:str,tasks:list[EvalConfig]):
-    pass
+from evaluation.evalConfig import EvalConfig
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from tqdm import tqdm
+
+
+def evaluate_predictions(prediction, answer):
+    if prediction is None:
+        return "reject"
+    if prediction == answer:
+        return "success"
+    return "fail"
+
+
+def print_results(task_name, stats):
+    total = sum(stats.values())
+    accuracy = stats["success"] / total if total > 0 else 0
+
+    print(f"\n\033[94m{task_name} results\033[0m")
+    print("\033[94mSuccess:\033[0m", stats["success"])
+    print("\033[94mFail:\033[0m", stats["fail"])
+    print("\033[94mReject:\033[0m", stats["reject"])
+    print("\033[94mAccuracy:\033[0m", accuracy)
+
+
+def Evaluate(model_id: str,lora_dir: str, tasks: list[EvalConfig]):
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tg_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+    tokenizer.padding_side = "left"
+
+    for task in tasks:
+        formatted_dataset = task.dataset.map(task.data_formatter)
+        prompts = formatted_dataset["prompt"]
+        answers = formatted_dataset["answer"]
+
+        responses_raw = []
+        batch_size = 8
+
+        print(f"\nStarting {task.name} inference.")
+        for i in tqdm(
+            range(0, len(prompts), batch_size),
+            desc=f"Generating {task.name}",
+        ):
+            batch = prompts[i:i + batch_size]
+            out = tg_pipeline(
+                batch,
+                max_new_tokens=task.token_limit,
+                batch_size=batch_size,
+            )
+            responses_raw.extend(out)
+        print(f"{task.name} inference finished.")
+
+        response_chats = [resp[0]["generated_text"] for resp in responses_raw]
+        predictions = [task.eval_fn(response_chat) for response_chat in response_chats]
+        results = [
+            evaluate_predictions(prediction, answer)
+            for prediction, answer in zip(predictions, answers)
+        ]
+
+        stats = Counter(results)
+        print_results(task.name, stats)
