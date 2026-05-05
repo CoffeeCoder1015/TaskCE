ad38b07eddcf08325fdcefffa51e4b46b2bcc9ae
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-05-05 15:01:15 -0700

    Formula class datastructure

--- feature/__init__.py
+++ feature/__init__.py
@@ -4,9 +4,14 @@ from feature.find import (
     filter_token_ids,
     top_token_counts,
 )
+from feature.formula import And, Leaf, Not, Or
 from feature.skip import SKIP_TOKENS, token_should_be_skipped
 
 __all__ = [
+    "And",
+    "Leaf",
+    "Not",
+    "Or",
     "SKIP_TOKENS",
     "batched",
     "count_model_token_ids",
