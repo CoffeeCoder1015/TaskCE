821dcf20241cdc847e7e2deb7b20818f0a552a10
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-05-06 02:08:52 -0700

    postprocessing functions to create binary activation functions

---
+++ capture/postprocessing.py
@@ -0,0 +1,18 @@
+# Post processing of raw activation vectors
+import torch
+
+
+def threshold(activation, alpha=None):
+    # shape = activation.shape  # [example count x activation size]
+    if alpha is None:
+        return (activation > 0).to(torch.int8)
+
+    thresholds = torch.quantile(activation, 1 - alpha, dim=0)
+    return (activation > thresholds).to(torch.int8)
+
+
+def prune_min_acts(binary_acts, min_acts=500):
+    frequency = binary_acts.sum(dim=0)
+    keep_mask = frequency >= min_acts
+    neuron_ids = torch.nonzero(keep_mask, as_tuple=False).flatten()
+    return binary_acts[:, keep_mask], neuron_ids
