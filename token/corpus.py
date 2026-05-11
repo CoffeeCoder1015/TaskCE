598ae010bb5187f2922e5e8f2474e1395e37f8f8
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-05-10 20:55:02 -0700

    toy custom tokenization example using HF tokenization

---
+++ token/corpus.py
@@ -0,0 +1,5 @@
+import datasets
+
+def build_corpus_from_dataset(dataset:datasets.Dataset):
+    # For consistency, it will only look for the "text" column and preprocessing will be done upstream by the user
+    pass
